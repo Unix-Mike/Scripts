8dce8118a1034af1df73e035f1f561dccd9b541e
Mike Studer <13511008+Unix-Mike@users.noreply.github.com>
2025-01-31 16:40:49 -0800

    Add files via upload
    add dd.sh command

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # Scripts
 Useful shell scripts 
 
-Script name       Purpose
-===========       =======
-colors.sh         Displays terminal colors with respective code
-asciiquarium      Ascii screen saver written in Perl, needs additional CPAN module
+Script name; Purpose
+colors.sh; Displays terminal colors with respective code
+dd.sh; Simple dd command with status. Requires gun pkg.
+
 
---
+++ dd.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/sh
+# Enhanced dd command
+# Written by Mike Studer Feb 2025
+#
+# Requires gum package
+#
+DD="/usr/bin/dd status=progress bs=4M"
+IF="input_file"
+OF="output_file"
+# Print fancy header
+gum style \
+	--foreground 212 --border-foreground 57 --border double \
+	--align center --width 50 --margin "1 1" \
+    'Simple dd command interface'
+# Now get user input
+IF=$(gum input --placeholder "Input File: ")
+OF=$(gum input --placeholder "Output File: ")
+
+CMD="$DD if=$IF of=$OF"
+gum style \
+	--foreground 11 \
+	--background 9 \
+    --border double \
+    --border-foreground 15 \
+	--align center --width 80 --margin "1 1" \
+    "Command preview: $CMD"
+gum confirm && $CMD
+
