bcef5cbc1a35e054c3e07b1cef0af2f81a179084
Mike Studer <mike@hamsoftware.org>
2025-02-02 13:29:40 -0800

    Rewrite of git_urls.sh. Much nicer

--- git_urls.sh
+++ git_urls.sh
@@ -1,2 +1,13 @@
 #!/bin/zsh
-find . -type f -name config -print| xargs -I{} sh -c 'grep url {}'|awk '{print $3}'
+while read -r file; do
+    echo ""
+    if [ -f "$file/.git/config" ]; then
+         Origin=$(cd $file;git remote get-url origin)
+         Repo=$(echo $file |sed 's/\.\///g')
+         echo "Repo = $Repo"
+         echo "URL = $Origin"
+         echo ""
+    else
+        echo "File does not exist: $file"
+    fi
+done < <(find . -type d -maxdepth 1 -mindepth 1 -print)
